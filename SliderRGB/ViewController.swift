ff581d9010d802c9d5b1fdc8ce4ec10b7ee9010a
Stanislav <stas.ace@gmail.com>
2024-02-04 19:19:00 +0500

    code: color change

--- SliderRGB/ViewController.swift
+++ SliderRGB/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var colorRGBView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorRGBView.layer.cornerRadius = CGFloat(15)
+        editColor()
     }
 
+    @IBAction func editValueSlider() {
+        if redValueLabel.text != String(format: "%.2f", redSlider.value) {
+            redValueLabel.text = String(format: "%.2f", redSlider.value)
+        } else if greenValueLabel.text != String(format: "%.2f", greenSlider.value) {
+            greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        } else {
+            blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        }
+        
+        editColor()
+    }
+    
+    private func editColor() {
+        colorRGBView.backgroundColor = UIColor.init(
+            displayP3Red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: CGFloat(1)
+        )
+ 
+    }
 
 }
 
